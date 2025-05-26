185b66b46a8fbbdfbd2cc7e1e2fa11f8d555012a
Calvin C Chan <hello@calvin-c.com>
2025-05-26 11:38:22 -0700

    refactor: simplify middleware path matching comments and update regex for clarity

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,9 @@ export default createMiddleware(routing);
 
 export const config = {
   matcher: [
-    // /*
-    //  * Match all request paths except for the ones starting with:
-    //  * - api (API routes)
-    //  * - _next/static (static files)
-    //  * - _next/image (image optimization files)
-    //  * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-    //  * - /assets and /images (static files)
-    //  */
-    // '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|assets|images).*)',
-
     // Match all pathnames except for
-    // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+    // - … if they start with `/api`, `/trpc`, `/_next`, `/_vercel`, `/images`, or `/assets`
     // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+    '/((?!api|trpc|_next|_vercel|images|assets|.*\\..*).*)'
   ],
 };
